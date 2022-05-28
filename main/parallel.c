74ba1b0e94d32ef4b3092af27b05e0cb787f1bbc
Oli Kabis <oli@kabis-online.de>
2022-05-28 15:59:06 +0200

    ddot_test

--- main/parallel.c
+++ main/parallel.c
@@ -1,126 +1,92 @@
 #include "hpc.h"
 #include "mesh_trans.h"
 #include <mpi.h>
+#include "blas_level1.h"
 
-double kappa( double x[2], index typ )
-{
-  return ( 1.0 );
-}
-
-double F_vol( double x[2], index typ )
-{
-  return ( 0.0 );
-}
-
-double g_Neu( double x[2], index typ )
-{
-  return ( x[0] * x[1] );
-}
-
-double u_D( double x[2])
-{
-//  return ( 0.0 );
-  return ( x[0] * x[1] );
-}
-
-void* make_global(index *c,index nlocal,double *rhs_loc,double *rhs_glob){
+double* make_global(index* c,double* r,double* rhs_loc,index nlocal){
   for(int i=0;i<nlocal;i++){
-    rhs_glob[c[i]] = rhs_loc[i];
+    rhs_loc[c[i]] = r[i];
   }
+  return rhs_loc;
 }
 
-void* make_local(index *c,index nlocal,double *rhs_glob,double *rhs_loc){
-  for(int i=0;i<nlocal;i++){
-    rhs_loc[i] = rhs_glob[c[i]] ;
+void* get_rhs_global(index* c, double* r, index nloc, double* rhs_loc, double* rhs_glob, index n){
+  MPI_Allreduce(
+    make_global(c,r,rhs_loc,nloc),
+    rhs_glob,
+    n,
+    MPI_DOUBLE,
+    MPI_SUM,
+    MPI_COMM_WORLD);
+}
+
+double* ddot_local(index* c, double* r, double* rhs_glob, index nloc, double* ddot_loc){
+  ddot_loc[0] = 0;
+  for(int i=0;i<nloc;i++){
+    ddot_loc[0] += rhs_glob[c[i]]*r[i];
   }
+  return ddot_loc;
+}
+
+double get_sigma(index* c, double* r, index nloc, index n){
+  double rhs_loc[n];
+  double rhs_glob[n];
+  get_rhs_global(c,r,nloc,rhs_loc,rhs_glob,n);
+  printf("\nrhs_loc = ");
+  for(int i=0;i<n;i++) printf("%f ", rhs_loc[i]);
+  printf("\nrhs_glob = ");
+  for(int i=0;i<n;i++) printf("%f ", rhs_glob[i]);
+  double ddot[1];
+  double ddot_loc[1];
+  MPI_Allreduce(
+    ddot_local(c,r,rhs_glob,nloc,ddot_loc),
+    ddot,
+    1,
+    MPI_DOUBLE,
+    MPI_SUM,
+    MPI_COMM_WORLD);
+  return ddot[0];
+}
+
+double get_sigma2(index* c, double* r, index nloc, index n){
+  double rhs_loc[n];
+  double rhs_glob[n];
+  get_rhs_global(c,r,nloc,rhs_loc,rhs_glob,n);
+  printf("\nrhs_loc = ");
+  for(int i=0;i<n;i++) printf("%f ", rhs_loc[i]);
+  printf("\nrhs_glob = ");
+  for(int i=0;i<n;i++) printf("%f ", rhs_glob[i]);
+  double ddot = blasl1_ddot(rhs_glob,rhs_glob,n);
+  return ddot;
 }
 
 int main(int argc, char *argv[]) {
 
   int numprocs;
 	int myid;
-	int i;
-  const int root=0;
 	MPI_Status stat;
 
   MPI_Init(&argc,&argv);
 	MPI_Comm_size(MPI_COMM_WORLD,&numprocs); /* find out how big the SPMD world is */
 	MPI_Comm_rank(MPI_COMM_WORLD,&myid); /* and this processes' rank is */
 
-  mesh_trans **metra;
-  index anz_dom = 2;
-  index ncoord = 9;
-  if (myid == 0){
-    
-    mesh* H = get_refined_mesh(1);
-
-    double* b1 = calloc(ncoord, sizeof(double));  
-    mesh_RHS(H, b1, F_vol, g_Neu); 
-    printf("\nProcessor %d rhs full mesh:\n", myid);
-    for(int i=0;i<ncoord;i++){
-      printf("%lg\n",b1[i]);
-    }
-
-    metra = malloc ( (anz_dom) * sizeof(mesh_trans));
-
-    for(size_t i=0;i<anz_dom;i++){
-      metra[i]=alloc_mesh_trans(anz_dom,ncoord);
-    }
-
-    meshsplit(H, metra, anz_dom);
-  }
-  mesh_trans* mesh_loc =  scatter_meshes(metra,MPI_COMM_WORLD,anz_dom,ncoord);
-
-  sed* S;
-  S = malloc (sizeof(sed));
-  S = sed_sm_build(mesh_loc);
-  /*printf("\nProcessor %d lokale SM:\n", myid);
-  sed_print(S,0);*/
-  double* b = calloc(mesh_loc->ncoord_loc, sizeof(double));
-  mesh_trans_rhs(mesh_loc,b,F_vol, g_Neu);
+  index n = 2*numprocs+1;
+  index nloc = 3;
+  index c[3] = { myid*2, myid*2+1, myid*2+2 };
+  double r[3] = { 1, 1, 1 };
 
   sleep(myid);
 
-  printf("\nProcessor %d rhs:\n", myid);
-  for(int i=0;i<mesh_loc->ncoord_loc;i++){
-    printf("%lg\n",b[i]);
-  }
+  printf("\n---Process %d---\n",myid);
+  printf("\nc: ");
+  for(int i=0;i<3;i++) printf("%d ",c[i]);
+  printf("\nr: ");
+  for(int i=0;i<3;i++) printf("%f ",r[i]);
+  printf("\n");
 
-  double* rhs_global = calloc(mesh_loc->ncoord_glo, sizeof(double));
-  make_global(mesh_loc->c,mesh_loc->ncoord_loc,b,rhs_global);
-  
-  
-  /*printf("\nProcessor %d rhs:\n", myid);
-  for(int i=0;i<mesh_loc->ncoord_loc;i++){
-    printf("%lg\n",b[i]);
-  }*/
-  printf("\nProcessor %d made global rhs:\n", myid);
-  for(int i=0;i<mesh_loc->ncoord_glo;i++){
-    printf("%lg\n",rhs_global[i]);
-  }
-
-
-  double recv_global[mesh_loc->ncoord_glo];
-  MPI_Allreduce(
-    rhs_global,
-    recv_global,
-    9,
-    MPI_DOUBLE,
-    MPI_SUM,
-    MPI_COMM_WORLD);
-  double* rhs_local = calloc(mesh_loc->ncoord_loc, sizeof(double));
-  make_local(mesh_loc->c,mesh_loc->ncoord_loc,rhs_global,rhs_local);
-
-  if(myid==0){
-    printf("\nProcessor %d rhs after allreduce:\n", myid);
-    for(int i=0;i<mesh_loc->ncoord_glo;i++){
-      printf("%lg\n",recv_global[i]);
-    }
-    /*printf("\nProcessor %d rhs after local:\n", myid);
-    for(int i=0;i<mesh_loc->ncoord_loc;i++){
-      printf("%lg\n",rhs_local[i]);
-    }*/
-  }
+  double sigma = get_sigma2(c,r,nloc,n);
+  printf("\nsigma = %f",sigma);
+  printf("\n");
 
   MPI_Finalize();
   return 0;
